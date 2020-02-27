c75265977c5f39400ac315a8a885999823b65fba
Vladimír Svoboda <svoboda-vlad@seznam.cz>
2020-02-27 09:52:16 +0100

    app.module - NgSelectModule import added

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -14,6 +14,7 @@ import { FooterComponent } from './layouts/footer/footer.component';
 import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
 import { ActiveMenuDirective } from './layouts/navbar/active-menu.directive';
 import { ErrorComponent } from './layouts/error/error.component';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @NgModule({
   imports: [
@@ -23,7 +24,8 @@ import { ErrorComponent } from './layouts/error/error.component';
     JhipsterDemoHomeModule,
     // jhipster-needle-angular-add-module JHipster will add new module here
     JhipsterDemoEntityModule,
-    JhipsterDemoAppRoutingModule
+    JhipsterDemoAppRoutingModule,
+    NgSelectModule
   ],
   declarations: [MainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, ActiveMenuDirective, FooterComponent],
   bootstrap: [MainComponent]
