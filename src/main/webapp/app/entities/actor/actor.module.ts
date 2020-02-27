4fd33627373bc6878ccced01bd41775a2ac72c63
Vladimír Svoboda <svoboda-vlad@seznam.cz>
2020-02-27 09:36:21 +0100

    actor.module - NgSelectModule import added

--- src/main/webapp/app/entities/actor/actor.module.ts
+++ src/main/webapp/app/entities/actor/actor.module.ts
@@ -7,9 +7,10 @@ import { ActorDetailComponent } from './actor-detail.component';
 import { ActorUpdateComponent } from './actor-update.component';
 import { ActorDeleteDialogComponent } from './actor-delete-dialog.component';
 import { actorRoute } from './actor.route';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @NgModule({
-  imports: [JhipsterDemoSharedModule, RouterModule.forChild(actorRoute)],
+  imports: [JhipsterDemoSharedModule, RouterModule.forChild(actorRoute), NgSelectModule],
   declarations: [ActorComponent, ActorDetailComponent, ActorUpdateComponent, ActorDeleteDialogComponent],
   entryComponents: [ActorDeleteDialogComponent]
 })
