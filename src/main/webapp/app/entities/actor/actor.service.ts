1ec99c5f6de64527f0f9a6d3017a202ee48e3721
Vladimír Svoboda <svoboda-vlad@seznam.cz>
2020-03-02 07:53:45 +0100

    Add entities using the JDL model `demo` (#3)
    * Add JDL Model `demo`
    
    See https://start.jhipster.tech/jdl-studio/#!/view/7b460d6b-94f8-49f1-8471-dc4ba478fbb1
    
    * Generate entities from JDL Model `demo`
    
    See https://start.jhipster.tech/jdl-studio/#!/view/7b460d6b-94f8-49f1-8471-dc4ba478fbb1
    
    Co-authored-by: JHipster Bot <jhipster-bot@users.noreply.github.com>

--- src/main/webapp/app/entities/actor/actor.service.ts
+++ src/main/webapp/app/entities/actor/actor.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import * as moment from 'moment';
 
+import { DATE_FORMAT } from 'app/shared/constants/input.constants';
 import { SERVER_API_URL } from 'app/app.constants';
 import { createRequestOption } from 'app/shared/util/request-util';
 import { IActor } from 'app/shared/model/actor.model';
@@ -16,23 +19,56 @@ export class ActorService {
   constructor(protected http: HttpClient) {}
 
   create(actor: IActor): Observable<EntityResponseType> {
-    return this.http.post<IActor>(this.resourceUrl, actor, { observe: 'response' });
+    const copy = this.convertDateFromClient(actor);
+    return this.http
+      .post<IActor>(this.resourceUrl, copy, { observe: 'response' })
+      .pipe(map((res: EntityResponseType) => this.convertDateFromServer(res)));
   }
 
   update(actor: IActor): Observable<EntityResponseType> {
-    return this.http.put<IActor>(this.resourceUrl, actor, { observe: 'response' });
+    const copy = this.convertDateFromClient(actor);
+    return this.http
+      .put<IActor>(this.resourceUrl, copy, { observe: 'response' })
+      .pipe(map((res: EntityResponseType) => this.convertDateFromServer(res)));
   }
 
   find(id: number): Observable<EntityResponseType> {
-    return this.http.get<IActor>(`${this.resourceUrl}/${id}`, { observe: 'response' });
+    return this.http
+      .get<IActor>(`${this.resourceUrl}/${id}`, { observe: 'response' })
+      .pipe(map((res: EntityResponseType) => this.convertDateFromServer(res)));
   }
 
   query(req?: any): Observable<EntityArrayResponseType> {
     const options = createRequestOption(req);
-    return this.http.get<IActor[]>(this.resourceUrl, { params: options, observe: 'response' });
+    return this.http
+      .get<IActor[]>(this.resourceUrl, { params: options, observe: 'response' })
+      .pipe(map((res: EntityArrayResponseType) => this.convertDateArrayFromServer(res)));
   }
 
   delete(id: number): Observable<HttpResponse<{}>> {
     return this.http.delete(`${this.resourceUrl}/${id}`, { observe: 'response' });
+  }
+
+  protected convertDateFromClient(actor: IActor): IActor {
+    const copy: IActor = Object.assign({}, actor, {
+      birthDate: actor.birthDate && actor.birthDate.isValid() ? actor.birthDate.format(DATE_FORMAT) : undefined
+    });
+    return copy;
+  }
+
+  protected convertDateFromServer(res: EntityResponseType): EntityResponseType {
+    if (res.body) {
+      res.body.birthDate = res.body.birthDate ? moment(res.body.birthDate) : undefined;
+    }
+    return res;
+  }
+
+  protected convertDateArrayFromServer(res: EntityArrayResponseType): EntityArrayResponseType {
+    if (res.body) {
+      res.body.forEach((actor: IActor) => {
+        actor.birthDate = actor.birthDate ? moment(actor.birthDate) : undefined;
+      });
+    }
+    return res;
   }
 }
