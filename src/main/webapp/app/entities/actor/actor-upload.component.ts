25c11f80ec3089b888ede28071a5ae70d5046bf2
Vladimír Svoboda <svoboda-vlad@seznam.cz>
2020-03-09 09:47:33 +0100

    actor-upload.component.ts file added.

---
+++ src/main/webapp/app/entities/actor/actor-upload.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { Papa } from "ngx-papaparse";
+
+import { HttpResponse } from '@angular/common/http';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { IActor, Actor } from 'app/shared/model/actor.model';
+import { ActorService } from './actor.service';
+
+@Component({
+  selector: 'jhi-actor-upload',
+  templateUrl: './actor-upload.component.html'
+})
+export class ActorUploadComponent {
+
+  dataList: any[];
+
+  constructor(private papa: Papa) {}
+
+  handleUpload($event: any) {
+    const fileList = $event.srcElement.files;
+    this.parseCsvFile(fileList[0]);
+  }
+
+  parseCsvFile(file) {
+    this.papa.parse(file, {
+      header: true,
+      dynamicTyping: true,
+      skipEmptyLines: "greedy",
+      worker: true,
+      complete: result => {
+        this.dataList = result.data;
+      }
+    });
+  }
+
+}
