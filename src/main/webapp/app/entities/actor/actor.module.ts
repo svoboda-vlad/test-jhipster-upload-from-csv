beef57349e22e2c89898f18b793f0b1032a489b7
Vladimír Svoboda <svoboda-vlad@seznam.cz>
2020-03-09 10:42:07 +0100

    actor.module.ts - added ActorUploadComponent

--- src/main/webapp/app/entities/actor/actor.module.ts
+++ src/main/webapp/app/entities/actor/actor.module.ts
@@ -5,12 +5,13 @@ import { JhipsterDemoSharedModule } from 'app/shared/shared.module';
 import { ActorComponent } from './actor.component';
 import { ActorDetailComponent } from './actor-detail.component';
 import { ActorUpdateComponent } from './actor-update.component';
+import { ActorUploadComponent } from './actor-upload.component';
 import { ActorDeleteDialogComponent } from './actor-delete-dialog.component';
 import { actorRoute } from './actor.route';
 
 @NgModule({
   imports: [JhipsterDemoSharedModule, RouterModule.forChild(actorRoute)],
-  declarations: [ActorComponent, ActorDetailComponent, ActorUpdateComponent, ActorDeleteDialogComponent],
+  declarations: [ActorComponent, ActorDetailComponent, ActorUpdateComponent, ActorDeleteDialogComponent, ActorUploadComponent],
   entryComponents: [ActorDeleteDialogComponent]
 })
 export class JhipsterDemoActorModule {}
