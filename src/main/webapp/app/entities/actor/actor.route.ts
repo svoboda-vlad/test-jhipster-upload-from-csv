ab920dc6cb01da9c9de8e5958d8a48ddb45a569c
Vladimír Svoboda <svoboda-vlad@seznam.cz>
2020-03-09 10:13:32 +0100

    actor.router.ts - added import ActorUploadComponent

--- src/main/webapp/app/entities/actor/actor.route.ts
+++ src/main/webapp/app/entities/actor/actor.route.ts
@@ -10,6 +10,8 @@ import { ActorService } from './actor.service';
 import { ActorComponent } from './actor.component';
 import { ActorDetailComponent } from './actor-detail.component';
 import { ActorUpdateComponent } from './actor-update.component';
+import { ActorUploadComponent } from './actor-upload.component';
+
 
 @Injectable({ providedIn: 'root' })
 export class ActorResolve implements Resolve<IActor> {
